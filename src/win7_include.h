f86aaf72385862ce50b3282967dadfb610ab77bc
Nicolae Ghimbovschi <xfreebird@gmail.com>
2011-01-28 15:25:04 +0200

    declared all ITaskbarList

--- src/win7_include.h
+++ src/win7_include.h
@@ -11,6 +11,15 @@
 
 //Definitions for Windows 7 Taskbar and JumpList
 
+typedef enum STPFLAG
+{
+    STPF_NONE = 0,
+    STPF_USEAPPTHUMBNAILALWAYS = 0x1,
+    STPF_USEAPPTHUMBNAILWHENACTIVE = 0x2,
+    STPF_USEAPPPEEKALWAYS = 0x4,
+    STPF_USEAPPPEEKWHENACTIVE = 0x8
+} STPFLAG;
+
 typedef enum THUMBBUTTONMASK
 {
     THB_BITMAP = 0x1,
@@ -121,15 +130,27 @@ typedef enum
 typedef GUID KNOWNFOLDERID;
 #define REFKNOWNFOLDERID const KNOWNFOLDERID &
 
-//MIDL_INTERFACE("ea1afb91-9e28-4b86-90e9-9e9f8a5eefaf")
-DECLARE_INTERFACE_(ITaskbarList3,IUnknown)
+//MIDL_INTERFACE("56FDF342-FD6D-11d0-958A-006097C9A090")
+DECLARE_INTERFACE_(ITaskbarList, IUnknown)
 {
     STDMETHOD (HrInit) (THIS) PURE;
     STDMETHOD (AddTab) (THIS_ HWND hwnd) PURE;
     STDMETHOD (DeleteTab) (THIS_ HWND hwnd) PURE;
     STDMETHOD (ActivateTab) (THIS_ HWND hwnd) PURE;
     STDMETHOD (SetActiveAlt) (THIS_ HWND hwnd) PURE;
+};
+typedef ITaskbarList *LPITaskbarList;
+
+//MIDL_INTERFACE("602D4995-B13A-429b-A66E-1935E44F4317")
+DECLARE_INTERFACE_(ITaskbarList2, ITaskbarList)
+{
     STDMETHOD (MarkFullscreenWindow) (THIS_ HWND hwnd, int fFullscreen) PURE;
+};
+typedef ITaskbarList2 *LPITaskbarList2;
+
+//MIDL_INTERFACE("ea1afb91-9e28-4b86-90e9-9e9f8a5eefaf")
+DECLARE_INTERFACE_(ITaskbarList3, ITaskbarList2)
+{
     STDMETHOD (SetProgressValue) (THIS_ HWND hwnd, ULONGLONG ullCompleted, ULONGLONG ullTotal) PURE;
     STDMETHOD (SetProgressState) (THIS_ HWND hwnd, TBPFLAG tbpFlags) PURE;
     STDMETHOD (RegisterTab) (THIS_ HWND hwndTab,HWND hwndMDI) PURE;
@@ -144,6 +165,13 @@ DECLARE_INTERFACE_(ITaskbarList3,IUnknown)
     STDMETHOD (SetThumbnailClip) (THIS_ HWND hwnd, RECT *prcClip) PURE;
 };
 typedef ITaskbarList3 *LPITaskbarList3;
+
+//MIDL_INTERFACE("c43dc798-95d1-4bea-9030-bb99e2983a1a")
+DECLARE_INTERFACE_(ITaskbarList4, ITaskbarList3)
+{
+    STDMETHOD (SetTabProperties) (HWND hwndTab, STPFLAG stpFlags) PURE;
+};
+typedef ITaskbarList4 *LPITaskbarList4;
 
 //MIDL_INTERFACE("92CA9DCD-5622-4bba-A805-5E9F541BD8C9")
 DECLARE_INTERFACE_(IObjectArray, IUnknown)
@@ -229,7 +257,10 @@ DEFINE_GUID_(CLSID_ShellItem,0x9ac9fbe1,0xe0a2,0x4ad6,0xb4,0xee,0xe2,0x12,0x01,0
 DEFINE_GUID_(CLSID_ApplicationDocumentLists,0x86bec222,0x30f2,0x47e0,0x9f,0x25,0x60,0xd1,0x1c,0xd7,0x5c,0x28);
 DEFINE_GUID_(CLSID_ApplicationDestinations,0x86c14003,0x4d6b,0x4ef3,0xa7,0xb4,0x05,0x06,0x66,0x3b,0x2e,0x68);
 
+DEFINE_GUID_(IID_ITaskbarList,0x56FDF342,0xFD6D,0x11d0,0x95,0x8A,0x00,0x60,0x97,0xC9,0xA0,0x90);
+DEFINE_GUID_(IID_ITaskbarList2,0x602D4995,0xB13A,0x429b,0xA6,0x6E,0x19,0x35,0xE4,0x4F,0x43,0x17);
 DEFINE_GUID_(IID_ITaskbarList3,0xea1afb91,0x9e28,0x4b86,0x90,0xE9,0x9e,0x9f,0x8a,0x5e,0xef,0xaf);
+DEFINE_GUID_(IID_ITaskbarList4,0xc43dc798,0x95d1,0x4bea,0x90,0x30,0xbb,0x99,0xe2,0x98,0x3a,0x1a);
 DEFINE_GUID_(IID_IObjectArray,0x92ca9dcd,0x5622,0x4bba,0xa8,0x05,0x5e,0x9f,0x54,0x1b,0xd8,0xc9);
 DEFINE_GUID_(IID_ICustomDestinationList,0x6332debf,0x87b5,0x4670,0x90,0xc0,0x5e,0x57,0xb4,0x08,0xa4,0x9e);
 DEFINE_GUID_(IID_IObjectCollection,0x5632b1a4,0xe38a,0x400a,0x92,0x8a,0xd4,0xcd,0x63,0x23,0x02,0x95);
