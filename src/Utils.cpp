f8b4d3cd13b0021f8031dc57d7ae9809088a8999
Nicolae Ghimbovschi <xfreebird@gmail.com>
2011-04-01 10:28:20 +0300

    add QtGlobal include in Utils.cpp

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -17,6 +17,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <QtGlobal>
+
 #ifdef Q_OS_WIN32
 
 #include "Utils.h"
